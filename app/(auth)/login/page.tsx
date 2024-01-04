deafc286a98b1f59afdb221ff86c1c39f6def645
Marcus Vinicius <5ucr4m@gmail.com>
2024-01-04 09:05:09 -0300

    chore: create folder structure

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+function SignIn() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Teste</h1>
+    </main>
+  );
+}
+
+export default SignIn;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Teste de registro</h1>
+    </main>
+  );
+}
+
+export default Register;
