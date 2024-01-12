6c5e0c732d566fe4e702673ac39967188da8b9bd
Marcus Vinicius <5ucr4m@gmail.com>
2024-01-11 21:30:27 -0300

    chore: create core upload api

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+import { isTeacher } from "@/lib/teacher";
  
 const f = createUploadthing();
  
@@ -10,24 +12,19 @@ const handleAuth = () => {
     }
     return { userId };
 } 
- 
-// (req, res, { image }) => {}
 
 export const ourFileRouter = {
-    courseImage: 
-        f({ image: { maxFileSize: "4MB", maxFileCount: 1 }})
-        .middleware(() => auth())
-        .onUploadComplete(() => {}),
-
-    couseAttachment: 
-        f(["text", "image", "video", "audio", "pdf"])
-        .middleware(() => auth())
-        .onUploadComplete(() => {}),
-        
-    chapterVideo: 
-        f({ video: { maxFileSize: "1GB", maxFileCount: 1 } })
-        .middleware(() => auth())
-        .onUploadComplete(() => {}),
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete((...args) => {
+      console.log(args);
+    }),
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "1GB" } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {})
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
